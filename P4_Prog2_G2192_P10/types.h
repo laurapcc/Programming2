a0404bee8161ebc114b7e2a9aef9f11a957262f1
Laura de Paz <lauradepaz@MacBook-Pro-de-Laura.local>
2019-04-23 12:10:36 +0200

    Carpeta p4 creada

---
+++ P4_Prog2_G2192_P10/types.h
@@ -0,0 +1,12 @@
+/*
+ * File:   types.h
+ * Author: Paula Samper, Laura de Paz
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef enum {ERROR = 0, OK = 1} Status;
+typedef enum {FALSE = 0, TRUE = 1} Bool;
+
+#endif /* TYPES_H */
